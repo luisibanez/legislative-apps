f77e019f593a5aa6ac2539b091890e2368b16b59
Luis Ibanez <luis.ibanez@kitware.com>
2012-11-15 16:39:06 -0500

    Added script to get Transcripts

---
+++ Sandbox/getTranscript.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import requests
+import json
+import sys
+
+if len(sys.argv) < 2:
+  sys.exit('Missing Argument. The arguments should be <regular|special> Date: such as: regular 08-03-2011')
+
+transcriptID=sys.argv[1]+'-session-'+sys.argv[2]
+
+answerformat='json'
+host='http://open.nysenate.gov/legislation/2.0/transcript/'
+
+requesturl=host+transcriptID+'.'+answerformat
+
+print requesturl
+
+response = requests.get(requesturl)
+
+print json.dumps(response.json)
+
