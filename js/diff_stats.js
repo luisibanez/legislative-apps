11757e1bece8ad48369886f31dfe9adde9d02e74
Luis Ibanez <luis.ibanez@kitware.com>
2012-11-16 14:42:20 -0500

    Computing diff statistics between two bills.
    Function for computing the diff statistics between two bills.

---
+++ js/diff_stats.js
@@ -0,0 +1,14 @@
+﻿/**
+  *  Notes -  This function computes statistics on the number of differences between two bills.
+  * 
+  *  It is inspired in the behavior of the Git command
+  *
+  *                          git diff --stat
+  * 
+  **/
+
+/*
+ *  Getting two bills
+ */
+
+
